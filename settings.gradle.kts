acff6b2daac773deb55586e3b165f278e4ceb58d
Muhammet KDR <mskdr1453@gmail.com>
2023-07-15 18:48:16 +0300

    module structure has been created

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,9 @@ dependencyResolutionManagement {
 
 rootProject.name = "MovieApp"
 include(":app")
+include(":core:common")
+include(":core:data")
+include(":core:domain")
+include(":core:ui")
+include(":feature:home")
+include(":navigation")
