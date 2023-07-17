4460f1c589745d07f5ebab31344178e24069cfa2
Muhammet KDR <mskdr1453@gmail.com>
2023-07-17 22:04:08 +0300

    movie api service created

---
+++ core/data/src/main/java/com/muhammetkudur/data/api/MovieService.kt
@@ -0,0 +1,25 @@
+package com.muhammetkudur.data.api
+
+import com.muhammetkudur.data.dto.MovieResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ *    Created By Muhammet KÜDÜR
+ *    17.07.2023
+ */
+
+interface MovieService {
+
+    @GET(TOP_RATED)
+    suspend fun fetchTopRatedMovies(
+        @Query(LANGUAGE) language: String,
+        @Query(PAGE) page: Int
+    ): MovieResponse
+
+    companion object {
+        private const val TOP_RATED = "movie/top_rated"
+        private const val PAGE = "page"
+        private const val LANGUAGE = "language"
+    }
+}
