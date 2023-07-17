8f4acbf6e823adb3bd32ceab452ee55c659afb09
Muhammet KDR <mskdr1453@gmail.com>
2023-07-17 22:09:04 +0300

    main activity has been created

--- app/src/main/java/com/muhammetkudur/movieapp/MainActivity.kt
+++ app/src/main/java/com/muhammetkudur/movieapp/MainActivity.kt
@@ -2,7 +2,14 @@ package com.muhammetkudur.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 
+/**
+ *    Created By Muhammet KÜDÜR
+ *    17.07.2023
+ */
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
