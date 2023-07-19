6582f5df1bbe0325f954bd93edc62a242a1ce199
Muhammet KDR <mskdr1453@gmail.com>
2023-07-19 20:54:20 +0300

    topRatedMovieResponse data class created for network response

---
+++ core/data/src/main/java/com/muhammetkudur/data/dto/TopRatedMovieResponse.kt
@@ -0,0 +1,13 @@
+package com.muhammetkudur.data.dto
+
+import com.squareup.moshi.Json
+
+/**
+ *    Created By Muhammet KÜDÜR
+ *    17.07.2023
+ */
+
+data class TopRatedMovieResponse(
+    @field:Json(name = "page") val page: Int,
+    @field:Json(name = "results") val results: List<TopRatedMovie>
+)
