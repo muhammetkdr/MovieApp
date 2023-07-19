e6771d3c24b3b5d3a7910882d1a6b6b7688f79ea
Muhammet KDR <mskdr1453@gmail.com>
2023-07-19 20:55:37 +0300

    topRatedMovieUseCase has been implemented

---
+++ core/domain/src/main/java/com/muhammetkudur/domain/usecase/GetTopRatedMoviesUseCaseImpl.kt
@@ -0,0 +1,18 @@
+package com.muhammetkudur.domain.usecase
+
+import androidx.paging.PagingData
+import com.muhammetkudur.domain.model.TopRatedMovieEntity
+import com.muhammetkudur.domain.repository.MovieRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+/**
+ *    Created By Muhammet KÜDÜR
+ *    17.07.2023
+ */
+
+class GetTopRatedMoviesUseCaseImpl @Inject constructor(private val movieRepository: MovieRepository) :
+    GetTopRatedMoviesUseCase {
+    override fun invoke(): Flow<PagingData<TopRatedMovieEntity>> =
+        movieRepository.fetchTopRatedMovies()
+}
