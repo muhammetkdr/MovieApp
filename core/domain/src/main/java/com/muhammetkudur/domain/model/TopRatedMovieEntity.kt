cc8545fa1797edb5bd44dd36f0fdf7149d9d7445
Muhammet KDR <mskdr1453@gmail.com>
2023-07-19 20:53:14 +0300

    topRatedEntity class has been created

---
+++ core/domain/src/main/java/com/muhammetkudur/domain/model/TopRatedMovieEntity.kt
@@ -0,0 +1,12 @@
+package com.muhammetkudur.domain.model
+
+/**
+ *    Created By Muhammet KÜDÜR
+ *    17.07.2023
+ */
+
+data class TopRatedMovieEntity(
+    val title: String,
+    val posterUrl: String,
+    val overview: String
+)
