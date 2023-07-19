e4a3b16e8915a308c892a93ac68cf81f63bd4784
Muhammet KDR <mskdr1453@gmail.com>
2023-07-19 20:52:08 +0300

    Renamed and added new json object for network response

---
+++ core/data/src/main/java/com/muhammetkudur/data/dto/TopRatedMovie.kt
@@ -0,0 +1,16 @@
+package com.muhammetkudur.data.dto
+
+import com.squareup.moshi.Json
+
+/**
+ *    Created By Muhammet KÜDÜR
+ *    17.07.2023
+ */
+
+data class TopRatedMovie(
+    @field:Json(name = "original_title") val title: String?,
+    @field:Json(name = "poster_path") val posterUrl: String?,
+    @field:Json(name = "popularity") val popularity: Double?,
+    @field:Json(name = "overview") val overview: String?,
+    @field:Json(name ="release_date") val releaseDate: String?
+)
