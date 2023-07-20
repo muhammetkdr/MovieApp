1ea41a9c0e27eeca860de3f5e5633281742875cb
Muhammet KDR <mskdr1453@gmail.com>
2023-07-20 19:11:25 +0300

    base adapter added

---
+++ core/ui/src/main/java/com/muhammetkudur/ui/base/BaseAdapter.kt
@@ -0,0 +1,61 @@
+package com.muhammetkudur.ui.base
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+
+/**
+ *    Created By Muhammet KÜDÜR
+ *    17.07.2023
+ */
+
+abstract class BaseAdapter<T : Any>(
+    private val itemLayoutRes: Int = -1,
+    diffCallback: DiffUtil.ItemCallback<T> = object : DiffUtil.ItemCallback<T>() {
+        override fun areItemsTheSame(oldItem: T, newItem: T): Boolean {
+            return false
+        }
+        override fun areContentsTheSame(oldItem: T, newItem: T): Boolean {
+            return false
+        }
+    }
+) : ListAdapter<T, BaseViewHolder<T>>(diffCallback) {
+
+    var callback: OnItemClickListener<T>? = null
+
+    abstract fun bind(itemView: View, item: T, position: Int, viewHolder: BaseViewHolderImpl)
+
+    inner class BaseViewHolderImpl(itemView: View) : BaseViewHolder<T>(itemView) {
+        override fun bind(item: T, position: Int) {
+            this@BaseAdapter.bind(itemView, item, position, this)
+            callback?.let { notNullCallback ->
+                itemView.setOnClickListener {
+                    notNullCallback(item)
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<T> {
+        val view = LayoutInflater.from(parent.context).inflate(viewType, parent, false)
+        return BaseViewHolderImpl(view)
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder<T>, position: Int) {
+        holder.bind(getItem(position), position)
+    }
+
+    override fun getItemViewType(position: Int) = itemLayoutRes
+
+    open fun setOnItemClickListener(listener: OnItemClickListener<T>) {
+        callback = listener
+    }
+
+    fun clearList() {
+        submitList(arrayListOf())
+    }
+}
+
+typealias OnItemClickListener<T> = (data: T) -> Unit
