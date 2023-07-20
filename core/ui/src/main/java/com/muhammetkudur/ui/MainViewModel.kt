969f4c60504e89500b7baba89f446768d2d4af63
Muhammet KDR <mskdr1453@gmail.com>
2023-07-20 19:18:18 +0300

    mainViewModel created

---
+++ core/ui/src/main/java/com/muhammetkudur/ui/MainViewModel.kt
@@ -0,0 +1,15 @@
+package com.muhammetkudur.ui
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+/**
+ *    Created By Muhammet KÜDÜR
+ *    17.07.2023
+ */
+
+@HiltViewModel
+class MainViewModel @Inject constructor(): ViewModel(){
+
+}
