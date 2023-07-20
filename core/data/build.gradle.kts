737ec528f2b48534fec6004ffcaafaca980539e5
Muhammet KDR <mskdr1453@gmail.com>
2023-07-20 17:37:49 +0300

    paging lib. migrated to ktx

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -63,7 +63,7 @@ dependencies {
     kapt("com.google.dagger:hilt-compiler:2.45")
 
     //Paging
-    implementation("androidx.paging:paging-runtime:3.1.1")
+    implementation("androidx.paging:paging-runtime-ktx:3.1.1")
 
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
