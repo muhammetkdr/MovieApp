cbfa8163b18939223feb7bbf0fc3570b6e6ee900
Muhammet KDR <mskdr1453@gmail.com>
2023-07-20 19:16:30 +0300

    topRatedMovieUiData data class created

---
+++ core/ui/src/main/java/com/muhammetkudur/ui/model/TopRatedMovieUiData.kt
@@ -0,0 +1,12 @@
+package com.muhammetkudur.ui.model
+
+/**
+ *    Created By Muhammet KÜDÜR
+ *    17.07.2023
+ */
+
+data class TopRatedMovieUiData(
+    val title: String,
+    val posterUrl: String,
+    val overview: String
+)
