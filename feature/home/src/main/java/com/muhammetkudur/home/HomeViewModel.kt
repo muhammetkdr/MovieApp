2020f23abcfcdc406b0db783d76cefe0712cb523
Muhammet KDR <mskdr1453@gmail.com>
2023-07-20 19:18:55 +0300

    HomeViewModel has been created

---
+++ feature/home/src/main/java/com/muhammetkudur/home/HomeViewModel.kt
@@ -0,0 +1,36 @@
+package com.muhammetkudur.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import androidx.paging.map
+import com.muhammetkudur.domain.mapper.Mapper
+import com.muhammetkudur.domain.model.TopRatedMovieEntity
+import com.muhammetkudur.domain.usecase.GetTopRatedMoviesUseCase
+import com.muhammetkudur.ui.model.TopRatedMovieUiData
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+/**
+ *    Created By Muhammet KÜDÜR
+ *    17.07.2023
+ */
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val getTopRatedMoviesUseCase: GetTopRatedMoviesUseCase,
+    private val topRatedUiDataMapper: Mapper<TopRatedMovieEntity,TopRatedMovieUiData>
+): ViewModel() {
+
+    fun getTopRatedMoviesData() : Flow<PagingData<TopRatedMovieUiData>>{
+        return getTopRatedMoviesUseCase.invoke().map { pagingTopRatedData ->
+            pagingTopRatedData.map {topRatedEntity->
+                topRatedUiDataMapper.map(topRatedEntity)
+            }
+        }.cachedIn(viewModelScope)
+    }
+
+}
