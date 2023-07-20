a1d1bc4b7b0c1bc1416da9724ec0347179c633ba
Muhammet KDR <mskdr1453@gmail.com>
2023-07-20 19:19:09 +0300

    TopRatedUiDataMapper implemented

---
+++ feature/home/src/main/java/com/muhammetkudur/home/mapper/HomeTopRatedUiDataMapperImpl.kt
@@ -0,0 +1,21 @@
+package com.muhammetkudur.home.mapper
+
+import com.muhammetkudur.domain.mapper.Mapper
+import com.muhammetkudur.domain.model.TopRatedMovieEntity
+import com.muhammetkudur.ui.model.TopRatedMovieUiData
+import javax.inject.Inject
+
+/**
+ *    Created By Muhammet KÜDÜR
+ *    17.07.2023
+ */
+
+class HomeTopRatedUiDataMapperImpl @Inject constructor() : Mapper<TopRatedMovieEntity,TopRatedMovieUiData> {
+    override fun map(input: TopRatedMovieEntity): TopRatedMovieUiData {
+        return TopRatedMovieUiData(
+            title = input.title,
+            posterUrl = input.posterUrl,
+            overview = input.overview
+        )
+    }
+}
