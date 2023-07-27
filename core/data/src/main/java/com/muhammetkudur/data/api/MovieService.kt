a9c4258eb6939bcb7fae208ae652fd95cea44ad3
Muhammet KDR <mskdr1453@gmail.com>
2023-07-27 22:27:09 +0300

    new request fetch movie by id added

--- core/data/src/main/java/com/muhammetkudur/data/api/MovieService.kt
+++ core/data/src/main/java/com/muhammetkudur/data/api/MovieService.kt
@@ -1,7 +1,10 @@
 package com.muhammetkudur.data.api
 
-import com.muhammetkudur.data.dto.TopRatedMovieResponse
+import com.muhammetkudur.data.dto.Movie
+import com.muhammetkudur.data.dto.MovieResponse
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 /**
@@ -13,13 +16,22 @@ interface MovieService {
 
     @GET(TOP_RATED)
     suspend fun fetchTopRatedMovies(
-        @Query(LANGUAGE) language: String,
+        @Query(LANGUAGE_QUERY) language: String = LANGUAGE,
         @Query(PAGE) page: Int
-    ): TopRatedMovieResponse
+    ): MovieResponse
+
+    @GET(DETAIL)
+    suspend fun fetchMovieById(
+        @Path(PATH_MOVIE_ID) id: Int,
+        @Query(LANGUAGE_QUERY) language: String = LANGUAGE
+    ): Response<Movie>
 
     companion object {
         private const val TOP_RATED = "movie/top_rated"
+        private const val PATH_MOVIE_ID = "movie_id"
+        private const val DETAIL = "movie/{$PATH_MOVIE_ID}"
         private const val PAGE = "page"
-        private const val LANGUAGE = "language"
+        private const val LANGUAGE_QUERY = "language"
+        private const val LANGUAGE = "en-US"
     }
 }
