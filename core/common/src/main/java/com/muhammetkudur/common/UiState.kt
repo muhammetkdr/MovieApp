a00100b30b33561dfe58030b660ffde951e17a59
Muhammet KDR <mskdr1453@gmail.com>
2023-07-27 22:02:54 +0300

    UiState error state changed

--- core/common/src/main/java/com/muhammetkudur/common/UiState.kt
+++ core/common/src/main/java/com/muhammetkudur/common/UiState.kt
@@ -1,14 +1,12 @@
 package com.muhammetkudur.common
 
-import androidx.annotation.StringRes
-
 /**
  *    Created By Muhammet KÜDÜR
  *    17.07.2023
  */
 
 sealed class UiState<out T: Any> {
-    object Loading : UiState<Nothing>()
+    object Idle : UiState<Nothing>()
     data class Success<T: Any>(val data: T) : UiState<T>()
-    data class Error(@StringRes val message: Int) : UiState<Nothing>()
+    data class Error(val message: String) : UiState<Nothing>()
 }
