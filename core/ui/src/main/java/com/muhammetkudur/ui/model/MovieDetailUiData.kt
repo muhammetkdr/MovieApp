9821b61642cbc78c203059514acb66471b72fa80
Muhammet KDR <mskdr1453@gmail.com>
2023-07-27 22:21:32 +0300

    movie detail ui data created

---
+++ core/ui/src/main/java/com/muhammetkudur/ui/model/MovieDetailUiData.kt
@@ -0,0 +1,12 @@
+package com.muhammetkudur.ui.model
+
+/**
+ *    Created By Muhammet KÜDÜR
+ *    17.07.2023
+ */
+
+data class MovieDetailUiData(
+    val title: String,
+    val posterUrl: String,
+    val overview: String,
+)
