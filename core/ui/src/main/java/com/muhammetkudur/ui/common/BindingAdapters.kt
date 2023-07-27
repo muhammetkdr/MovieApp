71e9af3bd7d1144b6bb8c8fe1791db41345138d1
Muhammet KDR <mskdr1453@gmail.com>
2023-07-27 21:56:19 +0300

    placeholder and error state added

--- core/ui/src/main/java/com/muhammetkudur/ui/common/BindingAdapters.kt
+++ core/ui/src/main/java/com/muhammetkudur/ui/common/BindingAdapters.kt
@@ -3,7 +3,8 @@ package com.muhammetkudur.ui.common
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
-import com.muhammetkudur.ui.utils.Constants.MOVIE_IMAGE_BASE_URL
+import com.muhammetkudur.ui.R
+import com.muhammetkudur.ui.utils.constants.Constants.MOVIE_IMAGE_BASE_URL
 
 /**
  *    Created By Muhammet KÜDÜR
@@ -13,5 +14,7 @@ import com.muhammetkudur.ui.utils.Constants.MOVIE_IMAGE_BASE_URL
 @BindingAdapter("setMovieImage")
 fun ImageView.setMovieImage(moviePath:String?) {
     Glide.with(this).load(MOVIE_IMAGE_BASE_URL + moviePath)
+        .placeholder(R.drawable.ic_downloading)
+        .error(R.drawable.ic_error)
         .into(this)
 }
