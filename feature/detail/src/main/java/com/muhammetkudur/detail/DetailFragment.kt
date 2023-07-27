cc3bd9f00ec70de7b5b0214b5735684f7e9e10a9
Muhammet KDR <mskdr1453@gmail.com>
2023-07-27 22:17:05 +0300

    DetailFragment added

---
+++ feature/detail/src/main/java/com/muhammetkudur/detail/DetailFragment.kt
@@ -0,0 +1,51 @@
+package com.muhammetkudur.detail
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import com.muhammetkudur.common.UiState
+import com.muhammetkudur.detail.databinding.FragmentDetailBinding
+import com.muhammetkudur.ui.base.BaseFragment
+import com.muhammetkudur.ui.common.collectFlow
+import com.muhammetkudur.ui.common.showSnackbar
+import com.muhammetkudur.ui.utils.extensions.inflate
+import dagger.hilt.android.AndroidEntryPoint
+
+/**
+ *    Created By Muhammet KÜDÜR
+ *    17.07.2023
+ */
+
+@AndroidEntryPoint
+class DetailFragment : BaseFragment(R.layout.fragment_detail) {
+
+    private val binder by inflate(FragmentDetailBinding::bind)
+    override val viewModel by viewModels<DetailViewModel>()
+    private val args: DetailFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeDetailUiData()
+        fetchDetailMovieData()
+    }
+
+    private fun fetchDetailMovieData() {
+        viewModel.fetchMovieDetailData(args.id)
+    }
+
+    private fun observeDetailUiData() {
+        collectFlow(viewModel.detailMovieData){
+            when (it) {
+                is UiState.Success -> {
+                    binder.detailUiData = it.data
+                }
+                is UiState.Error -> {
+                    requireView().showSnackbar(it.message)
+                }
+                UiState.Idle -> {}
+            }
+        }
+    }
+}
