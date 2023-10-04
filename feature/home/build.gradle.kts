228dc4911f03abc588ffd0c1b3e3d872a41ec504
Muhammet KDR <mskdr1453@gmail.com>
2023-10-04 21:02:59 +0300

    unnecessary module implementation has been removed

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -42,7 +42,6 @@ dependencies {
     implementation(project(":core:common"))
     implementation(project(":core:domain"))
     implementation(project(":core:ui"))
-    implementation(project(":core:data"))
     implementation(project(":navigation"))
 
     //Android
